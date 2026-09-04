0ad426cf913f2a10abf6b2707b2200f0de96150f
NoamRaich2012 <noamraich2012@gmail.com>
2026-09-04 23:05:44 +0300

    one step before func

--- main.dart
+++ main.dart
@@ -1,12 +1,19 @@
 
 import 'dart:io';
 void main() {
- 
-  File file = File('readings.json');
-  
+  //נתונים להמשך
+  String NCtemp ="";
+  String NCtime ="";
+  int i = 0;
+  //קריאת הקובץ ופירוק בתוך רשימה
+  File file = File('readings.json'); 
   String contents = file.readAsStringSync();
   List<String>list = contents.split(",");
-  String something = list[0];
-  List<String>list2 = something.split(":");
-  print("0");
+  //שימוש נתונים מההתחלה ופירוק ספיציפי לכל פארמטר
+  NCtime = list[i];
+  NCtemp = list[i+1];
+  String cleantime = NCtime.replaceAll(RegExp(r'[^A-Z0-9-:.]'), '');
+  String cleantemp = NCtemp.replaceAll(RegExp(r'[^A-Z0-9.]'), '');
+  print(cleantime);
+  print(cleantemp);
 }
