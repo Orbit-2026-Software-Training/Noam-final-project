e159ff7751d22915b3538b3475a9131b63aaf677
NoamRaich2012 <noamraich2012@gmail.com>
2026-09-10 19:48:14 +0300

    final without regexp stage 3

--- main.dart
+++ main.dart
@@ -8,30 +8,28 @@ void main()async{
   String contents = file.readAsStringSync();
   List<String>list = contents.split(",");
   List<String> list2 = [];
-  List<String>jokeList = [];
   List<String> jokesList = [];
-    for (int i = 0; i < list.length; i+=2){
-  NCtime = list[i];
-  NCtemp = list[i+1];
-  String cleantime = NCtime.replaceAll(RegExp(r'[^A-Z0-9-:.]'), '');
-  String cleantemp = NCtemp.replaceAll(RegExp(r'[^A-Z0-9.]'), '');
+  for (int i = 0; i < list.length; i+=2){
+    NCtime = list[i];
+    NCtemp = list[i+1];
+    String cleantime = NCtime.replaceAll(RegExp(r'[^A-Z0-9-:.]'), '');
+    String cleantemp = NCtemp.replaceAll(RegExp(r'[^A-Z0-9.]'), '');
       if (cleantime[0] == ":"){
         cleantime = cleantime.substring(1);
       }   
-      list2.add(cleantemp);
+    list2.add(cleantemp);
     }
     Future<void> apiReading() async {
       for (int i = 0; i < 10; i++){
         var url = Uri.parse('https://v2.jokeapi.dev/joke/Misc,Programming?format=xml&safe-mode&type=single');
         var response = await http.get(url);  
         String urlAsString = response.body.toString(); 
-  
-        String cleanUrl = urlAsString.replaceAll(RegExp(r'/'), '');
+        String cleanUrl = urlAsString.replaceAll("</joke>", "<joke>");
         List<String>JokeList = cleanUrl.split("<joke>");
         jokesList.add(JokeList[1]);
+
       }
-    //print(jokesList);
-    //print (jokesList.length);
+    
   
 }
       
@@ -46,7 +44,7 @@ void main()async{
     double CurrentMax = 0;
     int currentMaxLetters = 0;
     int j = 0;
-    
+    int jokeLength =0;
     for (int i = 0; i < list2.length; i++){
       double list = double.parse(list2[i]);
       if (list > CurrentMax){
@@ -55,8 +53,8 @@ void main()async{
     }
     
     for ( int i= 0; i < jokesList.length; i++){
-      String clearJoke = jokesList[i].replaceAll(RegExp(r'[^a-zA-Z0-9]'), '');
-      int jokeLength = clearJoke.length;
+      String clearJoke =jokesList[i].replaceAll("[^a-zA-Z0-9]", "");
+      jokeLength = clearJoke.length;
       if (jokeLength > currentMaxLetters){
         j = i;
         currentMaxLetters = jokeLength;
@@ -69,6 +67,7 @@ void main()async{
   void MinTempShortestLetters(List<String>list2,List<String>jokesList){
     double currentMin = 1000;
     int currentMinLetters = 1000;
+    int jokeLength = 0;
     int j = 0;
     for (int i = 0; i < list2.length; i++){
       double list = double.parse(list2[i]);
@@ -77,20 +76,23 @@ void main()async{
       }
     }
     for (int i = 0; i<jokesList.length;i++){
-      String clearJoke = jokesList[i].replaceAll(RegExp(r'[^a-zA-Z0-9]'), '');
-      int jokeLength = clearJoke.length;
+      String clearJoke =jokesList[i].replaceAll("[^a-zA-Z0-9]", "");
+      jokeLength = clearJoke.length;
+        jokeLength = clearJoke.length;
+     
       if (jokeLength < currentMinLetters){
         j = i;
         currentMinLetters = jokeLength;
       }
-    }
+     }
     print("Joke with the least letters: ${jokesList[j]} with ${currentMinLetters} letters");
     print ("Min temperature: ${currentMin}");
   }
   void averagetempAverageLetters(List<String> list2,List<String>jokesList){
     double sum = 0;
     int lettersSum = 0;
-    bool check = false;
+    int jokeLength =0;
+    int i=0;
     for (int i =0; i<list2.length;i++){
       double list = double.parse(list2[i]);
       sum = sum + list;
@@ -98,25 +100,29 @@ void main()async{
     double average = sum / list2.length;
     print ("Average temperature: ${average}");
     for (int i = 0; i<jokesList.length;i++){
-      String clearJoke = jokesList[i].replaceAll(RegExp(r'[^a-zA-Z0-9]'), '');
-      int jokeLength = clearJoke.length;
+      if (jokesList[i]=="."||jokesList[i]=="!"||jokesList[i]=="@"||jokesList[i]=="#"||jokesList[i]=="%"||jokesList[i]=="^"||jokesList[i]=="&"||jokesList[i]=="*"||jokesList[i]=="("||jokesList[i]==")"||jokesList[i]==","||jokesList[i]==" "){
+        String clearJoke = jokesList[i].replaceAll(jokesList[i], "");
+        jokeLength = clearJoke.length;
+      }
       lettersSum = lettersSum + jokeLength;
    }
-   for (int i = 0; i<jokesList.length;i++){
-      String clearJoke = jokesList[i].replaceAll(RegExp(r'[^a-zA-Z0-9]'), '');
-      int jokeLength = clearJoke.length;
-      double doubleAverage = lettersSum/jokesList.length;
-      int averageOfLength = doubleAverage.toInt();
-      if (jokeLength==averageOfLength){
-        check = true;
-        print ("Joke with the exact average letters: ${jokesList[i]} with ${jokeLength} letters");
+   for (i = 0; i<jokesList.length;i++){
+      String clearJoke =jokesList[i].replaceAll("[^a-zA-Z0-9]", "");
+      jokeLength = clearJoke.length;
+        jokeLength = clearJoke.length;
       }
+      double lettersAverage = lettersSum/jokesList.length;
+      
+    if (jokeLength==lettersAverage){
+      print ("Joke with the exact average letters: ${jokesList[i]} with ${jokeLength} letters");
     }
-    if (check == false){
-      print ("No joke with the exact average letters");
+    else{
+      print ("there are no Joke with the exact average letters");
+    
+    
     }
-
   }
+  
 int tempAbove25(List<String> list2){
   int counter = 0;
   for (int i = 0; i<list2.length;i++){
